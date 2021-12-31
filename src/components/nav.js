b52a551c98e75f64bb4be8b7d364617c19df685c
Ramesh Goonetilleke <ragoonet@cabrillo.edu>
2021-12-31 15:45:30 -0800

    navbarm

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,18 +1,11 @@
 import * as React from 'react';
 import { GatsbyImage } from 'gatsby-plugin-image';
 import { graphql, useStaticQuery } from 'gatsby';
-import Container from './container';
 import Link from './link';
 
 const Navigation = () => {
-  const [menuHidden, setMenuHidden] = React.useState(true);
-  const { contentfulNavigation, site, contentfulNavigationLogo } = useStaticQuery(graphql`
+  const { contentfulNavigation, contentfulNavigationLogo, contentfulNavigationButton } = useStaticQuery(graphql`
     query {
-      site {
-        siteMetadata {
-          title
-        }
-      }
       contentfulNavigation(slug: { eq: "header-navigation" }) {
         navigationItems {
           __typename
@@ -38,10 +31,13 @@ const Navigation = () => {
           }
         }
       }
+      contentfulNavigationButton {
+        navigationButtonText
+        navigationButtonLink
+      }
     }
   `);
 
-console.log(contentfulNavigationLogo.logoFile.gatsbyImageData);
   return (
     <header class="bg-purple mb-4">
       <nav class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
@@ -55,44 +51,37 @@ console.log(contentfulNavigationLogo.logoFile.gatsbyImageData);
               }
               <span class="font-bold text-white text-3xl tracking-tight leading-relaxed wordmark">Animal Run</span>
             </a>
-            <div class="hidden ml-10 space-x-8 lg:block">
-              <a href="#" class="text-base font-medium text-white hover:text-indigo-50" key="Solutions">
-                Solutions
-              </a>
-
-              <a href="#" class="text-base font-medium text-white hover:text-indigo-50" key="Pricing">
-                Pricing
-              </a>
-
-              <a href="#" class="text-base font-medium text-white hover:text-indigo-50" key="Docs">
-                Docs
-              </a>
-
-              <a href="#" class="text-base font-medium text-white hover:text-indigo-50" key="Company">
-                Company
-              </a>
+            <div class="hidden ml-10 space-x-8 lg:block grow items-center">
+            {contentfulNavigation.navigationItems.map((item, i) => (
+              <Link
+                className="text-lg text-white hover:text-indigo-50"
+                to={item.slug ? `/${item.slug}` : item.url}
+                key={i}
+              >
+                {item.title}
+              </Link>
+            ))}
             </div>
           </div>
           <div class="ml-10 space-x-4">
-            <a href="#" class="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium text-indigo-600 hover:bg-indigo-50">Sign up</a>
+            <Link
+              className="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium text-indigo-600 hover:bg-indigo-50"
+              to={ contentfulNavigationButton.navigationButtonLink }
+            >
+              { contentfulNavigationButton.navigationButtonText }
+            </Link>
           </div>
         </div>
         <div class="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
-          <a href="#" class="text-base font-medium text-white hover:text-indigo-50" key="Solutions">
-            Solutions
-          </a>
-
-          <a href="#" class="text-base font-medium text-white hover:text-indigo-50" key="Pricing">
-            Pricing
-          </a>
-
-          <a href="#" class="text-base font-medium text-white hover:text-indigo-50" key="Docs">
-            Docs
-          </a>
-
-          <a href="#" class="text-base font-medium text-white hover:text-indigo-50" key="Company">
-            Company
-          </a>
+        {contentfulNavigation.navigationItems.map((item, i) => (
+          <Link
+            className="text-base font-medium text-white hover:text-indigo-50"
+            to={item.slug ? `/${item.slug}` : item.url}
+            key={i}
+          >
+            {item.title}
+          </Link>
+        ))}
         </div>
       </nav>
     </header>
